5a4b04657618da4954f6c9fbbf30918f9871f1b9
TanapaPalmer <tanapa.palmer@gmail.com>
2023-07-19 10:16:12 -0400

    change some html style

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,6 +1,6 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
-from flask_app.models import report, user, city , updates   
+from flask_app.models import report, user, city , update   
 
 import re
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
@@ -25,7 +25,7 @@ class User:
     def is_valid_user(data):
         is_valid = True
         query = "SELECT * FROM users WHERE email = %(email)s;"
-        results = connectToMySQL('did_you_know').query_db(query,data)
+        results = connectToMySQL('communityOnAir').query_db(query,data)
         if len(results) >= 1:
             flash("Email already taken.")
             is_valid = False
@@ -49,7 +49,7 @@ class User:
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('did_you_know').query_db(query)
+        results = connectToMySQL('communityOnAir').query_db(query)
         users = []
         for row in results:
             users.append(cls(row))
@@ -61,7 +61,7 @@ class User:
     @classmethod
     def save(cls,data):
         query = "INSERT INTO users (name, email, password) VALUES (%(name)s, %(email)s, %(password)s);"
-        return connectToMySQL('did_you_know').query_db(query, data)
+        return connectToMySQL('communityOnAir').query_db(query, data)
     
 # ---------------------------------------------------
 # GET EMAIL
@@ -69,7 +69,7 @@ class User:
     @classmethod
     def get_by_email(cls,data):
         query = "SELECT * FROM users WHERE email = %(email)s;"
-        results = connectToMySQL('did_you_know').query_db(query,data)
+        results = connectToMySQL('communityOnAir').query_db(query,data)
         if len(results) < 1:
             return False
         return cls(results[0])
@@ -80,5 +80,5 @@ class User:
     @classmethod
     def get_by_id(cls,data):
         query = "SELECT * FROM users WHERE users.id = %(id)s;"
-        results = connectToMySQL('did_you_know').query_db(query,data)
+        results = connectToMySQL('communityOnAir').query_db(query,data)
         return cls(results[0])
